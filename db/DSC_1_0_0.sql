3b37813f244aa974693274b7f8b52793206f1c5b
Aditya Dubey <emailadityadubey@gmail.com>
2021-03-25 00:44:12 +0530

    db file updated

--- db/DSC_1_0_0.sql
+++ db/DSC_1_0_0.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Mar 04, 2021 at 07:35 PM
+-- Generation Time: Mar 24, 2021 at 08:11 PM
 -- Server version: 10.4.11-MariaDB
 -- PHP Version: 7.4.4
 
@@ -22,6 +22,27 @@ SET time_zone = "+00:00";
 --
 CREATE DATABASE IF NOT EXISTS `dsc` DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci;
 USE `dsc`;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `list`
+--
+
+CREATE TABLE `list` (
+  `Tab_ID` int(200) NOT NULL,
+  `Name` text NOT NULL,
+  `Card_No` varchar(15) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data for table `list`
+--
+
+INSERT INTO `list` (`Tab_ID`, `Name`, `Card_No`) VALUES
+(1, 'Aditya Dubey', '1201-6120-1612'),
+(2, 'Rahul Dubey', '9201-6140-1511'),
+(3, 'Lpu Wale', '9191-6840-9711');
 
 -- --------------------------------------------------------
 
@@ -45,6 +66,7 @@ CREATE TABLE `users` (
 --
 
 INSERT INTO `users` (`ID`, `First_Name`, `Middle_Name`, `Last_Name`, `Profession`, `Post`, `Email`, `Password`) VALUES
+('b97d', 'Rahul', '', 'Dubey', 'Office', 'Head Department', 'r@w.com', '69af7f59'),
 ('d5b5', 'Aditya', '', 'Dubey', 'Doctor', 'Head Department', 'a@g.com', 'adi1234');
 
 --
@@ -52,10 +74,26 @@ INSERT INTO `users` (`ID`, `First_Name`, `Middle_Name`, `Last_Name`, `Profession
 --
 
 --
+-- Indexes for table `list`
+--
+ALTER TABLE `list`
+  ADD PRIMARY KEY (`Tab_ID`);
+
+--
 -- Indexes for table `users`
 --
 ALTER TABLE `users`
   ADD UNIQUE KEY `ID` (`ID`);
+
+--
+-- AUTO_INCREMENT for dumped tables
+--
+
+--
+-- AUTO_INCREMENT for table `list`
+--
+ALTER TABLE `list`
+  MODIFY `Tab_ID` int(200) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
